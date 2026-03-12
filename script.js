9cac999329b8c115dc2f687845ebac9afd99baa0
LaurenGCampos <14lauren.campos@gmail.com>
2026-03-12 18:24:28 -0300

    link de pagamento

--- script.js
+++ script.js
@@ -4,70 +4,70 @@ const produtos = [
     nome: "Pod Black Sheep 30K", 
     preco: 139.90, 
     img: "img/BlackSheep.jpg",
-    pagamento: "https://https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw.com"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 2, 
     nome: "Oxbar Magic 30K", 
     preco: 159.90, 
     img: "img/oxbar.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 3, 
     nome: "Airmez", 
     preco: 149.90, 
     img: "img/Airmez.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 4, 
     nome: "Elfbar 40K", 
     preco: 179.90, 
     img: "img/Elfbar40k.png",
-    pagamento: "https://https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 5, 
     nome: "Ignite V250", 
     preco: 169.90, 
     img: "img/Ignitev250.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 6, 
     nome: "Life Pod", 
     preco: 129.90, 
     img: "img/LifePod.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 7, 
     nome: "Puff Makers", 
     preco: 149.90, 
     img: "img/PUFFMAKERS.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 8, 
     nome: "Pyne Pod", 
     preco: 159.90, 
     img: "img/PynePod.png",
-    pagamento: "https://seulink8.https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 9, 
     nome: "Sex Addict", 
     preco: 139.90, 
     img: "img/SexAddict.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   },
   { 
     id: 10, 
     nome: "Waka", 
     preco: 169.90, 
     img: "img/Waka.png",
-    pagamento: "https://go.ironpayapp.com.br/rq0gtlifbf?_gl=1*1ff2deb*_gcl_au*MTgxNzI0ODkyLjE3Njk2MjMwODcuNzk2NDA4NzUwLjE3NzA5NDM1NDkuMTc3MDk0MzU2Mw"
+    pagamento: "https://link.mercadopago.com.br/podsstore001"
   }
 ];
 
